f1ab3910fdc638f36d172e4fa54063edf914d210
gj701763 <gj701763@gmail.com>
2024-08-23 18:41:02 +0530

    modified

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 const navDialog = document.getElementById('nav-dialog');
+
 function handleMenu() {
     navDialog.classList.toggle('hidden');
 }
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-        console.log(this)
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
